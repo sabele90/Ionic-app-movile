ac396db4764b252bc2c1e4c7ecb2ea543ca47d27
sabele90 <sabele@hotmail.es>
2024-02-04 22:52:12 +0000

    menu

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { Componente } from 'src/app/interfaces/interfaces';
+import { DataService } from '../../services/data.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-inicio',
@@ -6,76 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./inicio.page.scss'],
 })
 export class InicioPage implements OnInit {
-componentes: Componente[] = [
-  {
-    icon: 'american-football',
-    name: 'Action Sheet',
-    redirectTo: '/action-sheet', 
-  },
-  {
-    icon: 'logo-apple-appstore',
-    name: 'Alert',
-    redirectTo: '/alert', 
-  },
-  {
-    icon: 'beaker',
-    name: 'Avatar',
-    redirectTo: '/avatar', 
-  },
-  {
-    icon: 'radio-button-on',
-    name: 'Buttons and Router',
-    redirectTo: '/buttons', 
-  },
-  {
-    icon: 'card',
-    name: 'Cards',
-    redirectTo: '/card', 
-  },
-  {
-    icon: 'checkmark-circle-outline',
-    name: 'Checkbox',
-    redirectTo: '/check', 
-  },
-  {
-    icon: 'calendar',
-    name: 'DateTime',
-    redirectTo: '/date-time', 
-  },
-  {
-    icon: 'car',
-    name: 'Fabs',
-    redirectTo: '/fab', 
-  },
-  {
-    icon: 'grid',
-    name: 'Grid - Rows',
-    redirectTo: '/grid', 
-  },
-  {
-    icon: 'infinite',
-    name: 'Infinite Scroll',
-    redirectTo: '/infinite-scroll', 
-  },
-  {
-    icon: 'hammer',
-    name: 'Input - Forms',
-    redirectTo: '/input', 
-  },
-  {
-    icon: 'list',
-    name: 'List - Sliding',
-    redirectTo: '/list', 
-  },
-]
-  constructor() { }
+  componentes? : Observable<Componente[]>;
+ 
+  
+  constructor( private menuCtrl: MenuController,
+               private dataService : DataService) { }
 
-  ngOnInit() {
+  ngOnInit( ) {
+    this.componentes = this.dataService.getMenuOpts();
   }
+  toggleMenu(){
+    this.menuCtrl.toggle()
+    }
 
-}
-interface Componente {
-  icon: string;
-  name: string;
-  redirectTo: string;
 }
